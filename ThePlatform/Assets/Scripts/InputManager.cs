9bb25a641b77bbeeb862bd076e0fcba3d0f6380c
Mountain Cat <tomasz.banas123@gmail.com>
2023-03-08 01:25:41 +0100

    Initial commit

---
+++ ThePlatform/Assets/Scripts/InputManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
+
+public class InputManager : MonoBehaviour
+{
+    public event Action<Vector2> PlayerMovedFixed;
+    public event Action PlayerNotMovedFixed;
+
+    public event Action<Vector2> PlayerMoved;
+    public event Action PlayerNotMoved;
+
+    private InputActions _inputActions;
+
+    private void Awake()
+    {
+        _inputActions = new InputActions();
+        _inputActions.Player.Enable();
+    }
+
+    private void Update()
+    {
+        var move = _inputActions.Player.Move.ReadValue<Vector2>();
+        if (move.magnitude > 0)
+            PlayerMoved?.Invoke(move);
+        else
+            PlayerNotMoved?.Invoke();
+    }
+
+    private void FixedUpdate()
+    {
+        var move = _inputActions.Player.Move.ReadValue<Vector2>();
+        if (move.magnitude > 0)
+            PlayerMovedFixed?.Invoke(move);
+        else
+            PlayerNotMovedFixed?.Invoke();
+    }
+}
